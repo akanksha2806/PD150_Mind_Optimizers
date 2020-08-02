8476d5c47db0b626e29f670248b5f85677818bd7
kuja <36929301+kuja24@users.noreply.github.com>
2020-08-02 13:57:11 +0530

    updated places api

--- website/public/scripts/doc.js
+++ website/public/scripts/doc.js
@@ -1,6 +1,35 @@
 
 var api_key = '367e3770394baf9fa17513f197699ca3'; 
+var map;
+var service;
+var infowindow;
 
+function initialize(lat,long) {
+  var pyrmont = new google.maps.LatLng(lat,long);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    type: ['restaurant']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+  console.log(results);
+}
 
  
 
@@ -17,13 +46,16 @@ if ("geolocation" in navigator) {
         lon = position.coords.longitude;
         console.log(lat);
         console.log(lon);
-        //var resource_url = 'https://api.betterdoctor.com/2016-03-01/practices?location=' + parseFloat(lat) + ',' + parseFloat(lon) + ',10&skip=2&limit=10&user_key=367e3770394baf9fa17513f197699ca3';
-        //$.get(resource_url, function (data) {
-            // data: { meta: {<metadata>}, data: {<array[Practice]>} }
-        var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+       // var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
         document.getElementById('content-placeholder').style.visibility = "visible"
-            .innerHTML = template(data);
-        //});
+        //initialize(lat,lon);
+        // var resource_url = 'https://api.betterdoctor.com/2016-03-01/practices?location=' + parseFloat(lat) + ',' + parseFloat(lon) + ',10&skip=2&limit=10&user_key=367e3770394baf9fa17513f197699ca3';
+        // $.get(resource_url, function (data) {
+        //     data: { meta: {<metadata>}, data: {<array[Practice]>} }
+        // var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+        // document.getElementById('content-placeholder').style.visibility = "visible"
+        //     .innerHTML = template(data);
+        // });
     });
 
 }
